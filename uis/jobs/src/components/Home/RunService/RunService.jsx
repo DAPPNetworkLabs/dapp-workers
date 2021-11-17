976e71f5204c8a9a9a75c63925ffdb140a70e99a
Natanael Prudhomme <nat@liquidapps.io>
2021-11-17 13:29:01 -0400

    tab code over

--- uis/jobs/src/components/Home/RunService/RunService.jsx
+++ uis/jobs/src/components/Home/RunService/RunService.jsx
@@ -4,9 +4,9 @@ import classes from './RunService.module.scss';
 const RunService = (props) => {
     return (
         <div>
-        <input type="text" onChange={(event) => props.onChange(event,"runService")} name="jobId" placeholder="uint256 jobId"></input>
-        <input type="text" onChange={(event) => props.onChange(event,"runService")} name="port" placeholder="uint256 port"></input>
-        <input type="text" onChange={(event) => props.onChange(event,"runService")} name="dapps" placeholder="uint256 dapps"></input>
+            <input type="text" onChange={(event) => props.onChange(event,"runService")} name="jobId" placeholder="uint256 jobId"></input>
+            <input type="text" onChange={(event) => props.onChange(event,"runService")} name="port" placeholder="uint256 port"></input>
+            <input type="text" onChange={(event) => props.onChange(event,"runService")} name="dapps" placeholder="uint256 dapps"></input>
             <button onClick={props.runService}>Run Service</button>
         </div>
     );
--- uis/jobs/src/components/Home/SetDockerImage/SetDockerImage.jsx
+++ uis/jobs/src/components/Home/SetDockerImage/SetDockerImage.jsx
@@ -4,10 +4,10 @@ import classes from './SetDockerImage.module.scss';
 const SetDockerImage = (props) => {
     return (
         <div>
-        <input type="text" onChange={(event) => props.onChange(event,"setDockerImage")} name="setDockerImageName" placeholder="string imageName"></input>
-        <input type="text" onChange={(event) => props.onChange(event,"setDockerImage")} name="imageAddress" placeholder="string imageAddress"></input>
-        <input type="text" onChange={(event) => props.onChange(event,"setDockerImage")} name="imageHash" placeholder="string imageHash"></input>
-        <input type="text" onChange={(event) => props.onChange(event,"setDockerImage")} name="imageType" placeholder="string imageType"></input>
+            <input type="text" onChange={(event) => props.onChange(event,"setDockerImage")} name="setDockerImageName" placeholder="string imageName"></input>
+            <input type="text" onChange={(event) => props.onChange(event,"setDockerImage")} name="imageAddress" placeholder="string imageAddress"></input>
+            <input type="text" onChange={(event) => props.onChange(event,"setDockerImage")} name="imageHash" placeholder="string imageHash"></input>
+            <input type="text" onChange={(event) => props.onChange(event,"setDockerImage")} name="imageType" placeholder="string imageType"></input>
             <button onClick={props.setDockerImage}>Set Docker Image</button>
         </div>
     );
