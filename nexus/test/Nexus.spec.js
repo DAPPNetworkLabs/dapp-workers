243e0f35e7b2c7a79dc5c88e45e70fa8baa4ac94
Natanael Prudhomme <nat@liquidapps.io>
2021-12-17 13:53:30 -0400

    add some unit tests

--- nexus/test/Nexus.spec.js
+++ nexus/test/Nexus.spec.js
@@ -6,47 +6,82 @@ const { BigNumber } = require("ethers");
 describe("Nexus", function() {
   this.timeout(100000);
   const web3 = new Web3();
-  let owner, addr1, addr2, addr3, addr4, addr5, addrs;
+  let owner, addr1, addr2, addr3, dsp1, dsp2, addrs;
+  let dappTokenContract, nexusContract;
 
   before(async function() {
-    [owner, addr1, addr2, addr3, addr4, addr5, ...addrs] = await ethers.getSigners();
+    [owner, addr1, addr2, addr3, dsp1, dsp2, ...addrs] = await ethers.getSigners();
 
+    const dappTokenFactory = await ethers.getContractFactory("DappToken", addr1);
+    const nexusTokenFactory = await ethers.getContractFactory("Nexus", addr1);
+
+    dappTokenContract = await dappTokenFactory.deploy();
+    nexusContract = await nexusTokenFactory.deploy(dappTokenContract.address);
   });
 
-  it("Register job", async function() {
-    
+  it("Deprecate DSP", async function() {
+    await nexusContract.connect(dsp1).deprecateDSP();
+
+    const registeredDSPs = await nexusContract.registeredDSPs(dsp1.address);
+
+    expect(registeredDSPs.active).to.equal(false);
+    expect(registeredDSPs.endpoint).to.equal("");
   });
 
-  it("Register service", async function() {
-    
+  it("Register DSP", async function() {
+    await nexusContract.connect(dsp1).regDSP("https://dsp.address", 20);
+
+    const registeredDSPs = await nexusContract.registeredDSPs(dsp1.address);
+
+    expect(registeredDSPs.active).to.equal(true);
+    expect(registeredDSPs.endpoint).to.equal("https://dsp.address");
+    expect(registeredDSPs.claimableDapp.toString()).to.equal('0');
+    expect(registeredDSPs.gasFeeMult.toString()).to.equal('20');
   });
 
   it("Buy dapp gas", async function() {
-    
+    const dapps = ethers.utils.parseUnits("10000",4);
+    await dappTokenContract.mint(addr1.address, dapps);
+    await dappTokenContract.approve(nexusContract.address, dapps);
+    await nexusContract.buyGasFor(dapps, addr1.address, dsp1.address);
+
+    const dspData = await nexusContract.dspData(addr1.address, dsp1.address);
+
+    expect(dspData.amount).to.equal(dapps);
   });
 
   it("Sell dapp gas", async function() {
-    
+    const dapps = ethers.utils.parseUnits("10000",4);
+
+    await nexusContract.sellGas(dapps, dsp1.address);
+
+    const dspData = await nexusContract.dspData(addr1.address, dsp1.address);
+
+    expect(dspData.amount.toString()).to.equal('0');
   });
 
-  it("Set consumer permission", async function() {
-    
+  it("Register job image", async function() {
+    await nexusContract.connect(dsp1).setJobDockerImage("wasm-runner","","",20);
+
+    const dockerImage = await nexusContract.jobDockerImages("wasm-runner");
+
+    expect(dockerImage.image).to.equal("");
+    expect(dockerImage.owner).to.equal(dsp1.address);
+    expect(dockerImage.imageHash).to.equal("");
+    expect(dockerImage.jobFee.toString()).to.equal('20');
   });
 
-  it("Set quorum", async function() {
-    
-  });
+  it("Register service image", async function() {
+    await nexusContract.connect(dsp1).setServiceDockerImage("wasi-service","","",10,10,10);
 
-  it("Claim dsp dapp", async function() {
-    
-  });
+    const dockerImage = await nexusContract.serviceDockerImages("wasi-service");
 
-  it("Create job", async function() {
-    
-  });
-
-  it("Create service", async function() {
-    
+    expect(dockerImage.image).to.equal("");
+    expect(dockerImage.owner).to.equal(dsp1.address);
+    expect(dockerImage.imageHash).to.equal("");
+    expect(dockerImage.baseFee.toString()).to.equal('10');
+    expect(dockerImage.storageFee.toString()).to.equal('10');
+    expect(dockerImage.ioFee.toString()).to.equal('10');
   });
 
   it("Run job", async function() {
@@ -65,11 +100,15 @@ describe("Nexus", function() {
     
   });
 
-  it("Register DSP", async function() {
+  it("Set consumer permission", async function() {
     
   });
 
-  it("Deprecate DSP", async function() {
+  it("Set quorum", async function() {
+    
+  });
+
+  it("Claim dsp dapp", async function() {
     
   });
 
@@ -108,4 +147,12 @@ describe("Nexus", function() {
   it("Get dsp endpoint", async function() {
     
   });
+
+  // it("Create job", async function() {
+    
+  // });
+
+  // it("Create service", async function() {
+    
+  // });
 });
