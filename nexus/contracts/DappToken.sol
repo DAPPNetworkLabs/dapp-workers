9f1e4e986820240387ab2fa6e4cea448a2a4fa3c
Natanael Prudhomme <nat@liquidapps.io>
2021-12-17 13:52:08 -0400

    add dapp token

---
+++ nexus/contracts/DappToken.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DappToken is ERC20('Dapp Token', 'DAPP'), Ownable {
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
+    
+    function decimals() public view virtual override returns (uint8) {
+        return 4;
+    }
+}
