f0110f7da1067c9eef6ab1f4af30cba4174785f4
NatPDeveloper <natanael.prudhomme@gmail.com>
2022-03-18 12:02:08 -0400

    copy dsp cells

--- uis/jobs/src/components/UI/Dsps/Dsps.jsx
+++ uis/jobs/src/components/UI/Dsps/Dsps.jsx
@@ -8,13 +8,13 @@ const Dsps = (props) => {
         dsps = props.dsps.map((el,i)=>{
             return (
                 <tr>
-                    <td>{`${el.dsp.slice(0,4)}..${el.dsp.slice(-4)}`}</td>
-                    <td>{el.endpoint}</td>
-                    <td>{el.jobsCompleted}</td>
-                    <td>{el.jobErrors}</td>
-                    <td>{el.servicesCompleted}</td>
-                    <td>{el.serviceErrors}</td>
-                    <td>{Number(el.dappGas)/1e4}</td>
+                    <td onClick={() => {navigator.clipboard.writeText(`${el.dsp}`)}}>{`${el.dsp.slice(0,4)}..${el.dsp.slice(-4)}`}</td>
+                    <td onClick={() => {navigator.clipboard.writeText(`${el.endpoint}`)}}>{el.endpoint}</td>
+                    <td onClick={() => {navigator.clipboard.writeText(`${el.jobsCompleted}`)}}>{el.jobsCompleted}</td>
+                    <td onClick={() => {navigator.clipboard.writeText(`${el.jobErrors}`)}}>{el.jobErrors}</td>
+                    <td onClick={() => {navigator.clipboard.writeText(`${el.servicesCompleted}`)}}>{el.servicesCompleted}</td>
+                    <td onClick={() => {navigator.clipboard.writeText(`${el.serviceErrors}`)}}>{el.serviceErrors}</td>
+                    <td onClick={() => {navigator.clipboard.writeText(`${Number(el.dappGas)/1e4}`)}}>{Number(el.dappGas)/1e4}</td>
                 </tr>
             )
         })
