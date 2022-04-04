26a923162849246209d4a41f1f71c7836c6779fb
NatPDeveloper <natanael.prudhomme@gmail.com>
2022-04-04 12:57:26 -0400

    try resolve heroku path refresh issue

--- uis/jobs/app.js
+++ uis/jobs/app.js
@@ -9,5 +9,12 @@ app.use(express.static(path.join(__dirname, 'build')));
 const port = process.env.PORT || '8080';
 app.set('port', port);
 
+if (process.env.NODE_ENV === 'production') {
+    // Express serve up index.html file if it doesn't recognize route
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+}
+
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Running on localhost:${port}`));
