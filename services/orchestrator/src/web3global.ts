fe8ce7754945a791af504dc7d50edd2b92ea0d60
NatPDeveloper <ubuntu@ip-172-31-81-246.ec2.internal>
2022-05-30 19:35:17 +0000

    add reconnect

--- services/orchestrator/src/web3global.ts
+++ services/orchestrator/src/web3global.ts
@@ -1,4 +1,28 @@
 const Web3 = require('web3');
 console.log(`process.env.ETH_ADDR`,process.env.ETH_ADDR)
-export let provider = new Web3.providers.WebsocketProvider(process.env.ETH_ADDR);
+export let provider = new Web3.providers.WebsocketProvider(process.env.ETH_ADDR, {
+    reconnect: {
+        auto: true,
+        delay: 1000,
+        onTimeout: false,
+    },
+    timeout: 45000,
+    clientConfig: {
+        maxReceivedFrameSize: 50000000000,
+        maxReceivedMessageSize: 50000000000,
+        keepalive: true,
+        keepaliveInterval: 10000,
+        dropConnectionOnKeepaliveTimeout: true,
+        keepaliveGracePeriod: 30000,
+    }
+});
+provider.on("connect", () => {
+    console.log(`connected!`);
+});
+provider.on("reconnect", () => {
+    console.log(`reconnecting...`);
+});
+provider.on("close", () => {
+    console.log(`disconnected`);
+});
 export let web3 = new Web3(provider);
