39149b5468f5838b670e266d34dd210c75cf07c2
NatPDeveloper <ubuntu@ip-172-31-81-246.ec2.internal>
2022-06-02 15:26:56 +0000

    rm orchestrator service, fix api deployment, update scripts to use PWD as dir for k8s scripts

--- scripts/k8s/k8s_stop.sh
+++ scripts/k8s/k8s_stop.sh
@@ -1,7 +1,7 @@
-kubectl delete -f ipfs-data-persistentvolumeclaim.yaml,ipfs0-deployment.yaml,ipfs0-service.yaml --all=true
-kubectl delete -f postgres-deployment.yaml,postgres-service.yaml --all=true
-kubectl delete -f rust-compiler-deployment.yaml,git-cloner-deployment.yaml,monte-carlo-dice-deployment.yaml,runner-deployment.yaml,solidity-runner-deployment.yaml,wasi-service-deployment.yaml --all=true
-kubectl delete -f eth-deployment.yaml,eth-service.yaml --all=true
-kubectl delete -f orchestrator-deployment.yaml,orchestrator-service.yaml --all=true
-kubectl delete -f api-deployment.yaml,api-service.yaml --all=true
-kubectl delete -f nexus-deployment.yaml --all=true
+kubectl delete -f $PWD/k8s/test/ipfs-data-persistentvolumeclaim.yaml,$PWD/k8s/test/ipfs0-deployment.yaml,$PWD/k8s/test/ipfs0-service.yaml
+kubectl delete -f $PWD/k8s/test/postgres-deployment.yaml,$PWD/k8s/test/postgres-service.yaml
+kubectl delete -f $PWD/k8s/test/rust-compiler-deployment.yaml,$PWD/k8s/test/git-cloner-deployment.yaml,$PWD/k8s/test/monte-carlo-dice-deployment.yaml,$PWD/k8s/test/runner-deployment.yaml,$PWD/k8s/test/solidity-runner-deployment.yaml,$PWD/k8s/test/wasi-service-deployment.yaml
+kubectl delete -f $PWD/k8s/test/eth-deployment.yaml,$PWD/k8s/test/eth-service.yaml
+kubectl delete -f $PWD/k8s/test/orchestrator-deployment.yaml
+kubectl delete -f $PWD/k8s/test/api-deployment.yaml,$PWD/k8s/test/api-service.yaml
+kubectl delete -f $PWD/k8s/test/nexus-deployment.yaml
