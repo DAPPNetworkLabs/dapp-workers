bec31d8283aac9fc0746da54b91b5c8be5dbcb9e
NatPDeveloper <ubuntu@ip-172-31-81-246.ec2.internal>
2022-06-16 20:00:44 +0000

    rm log

--- services/orchestrator/src/web3global.ts
+++ services/orchestrator/src/web3global.ts
@@ -1,5 +1,4 @@
 const Web3 = require('web3');
-console.log(`process.env.ETH_ADDR`,process.env.ETH_ADDR)
 import { subscribe } from './index';
 
 export let provider = new Web3.providers.WebsocketProvider(process.env.ETH_ADDR, {
