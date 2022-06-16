4882d60d7f5b2493b22787686ea07d66ad82189c
NatPDeveloper <ubuntu@ip-172-31-81-246.ec2.internal>
2022-06-16 19:55:50 +0000

    add metrics-server, update k8s shell script

--- scripts/k8s/k8s.sh
+++ scripts/k8s/k8s.sh
@@ -1,13 +1,17 @@
+docker image prune -f
+
 minikube start --memory='5000MB' --mount --mount-string='/:/k8s' --cpus='4'
 
 $PWD/scripts/k8s/build_deploy.sh
 
 $PWD/scripts/deploy-roots.sh 
 
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
 for file in $PWD/k8s/test/* ; do
     envsubst < $file | kubectl apply -f -
 done
 
-sleep 150
+sleep 140
 
 $PWD/scripts/k8s/logs.sh
