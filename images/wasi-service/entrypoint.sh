5001ee5d20359c1f31eef8be6893b7b7be2a6078
NatPDeveloper <ubuntu@ip-172-31-81-246.ec2.internal>
2022-06-16 19:51:42 +0000

    convert wasi-service image to k8s args

--- images/wasi-service/entrypoint.sh
+++ images/wasi-service/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 # run unpack ipfs
-ipfs get --api="/dns/$IPFS_HOST/tcp/5001"  --output /tmp1 $1
+ipfs get --api="/dns/$IPFS_HOST/tcp/5001"  --output /tmp1 $ARG_0
 
 source /root/.bashrc
 
-node ./dist/index.js /tmp1/$2.wasm # > /tmp1/stdout.txt
+node ./dist/index.js /tmp1/$ARG_1.wasm # > /tmp1/stdout.txt
 
 # pin and post ipfs
 ipfs add -r --api="/dns/$IPFS_HOST/tcp/5001" /tmp1 -Q
