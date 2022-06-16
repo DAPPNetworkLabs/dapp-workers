74c8045ae81933dbddfec327147fae0c26ae3fa5
NatPDeveloper <ubuntu@ip-172-31-81-246.ec2.internal>
2022-06-16 19:48:15 +0000

    edit docker pull images

--- images/docker-pull/entrypoint.sh
+++ images/docker-pull/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 docker pull natpdev/git-cloner
-docker pull natpdev/monte-carlo-dice
+docker pull natpdev/monte-carlo
 docker pull natpdev/runner
 docker pull natpdev/rust-compiler
 docker pull natpdev/solidity-runner
 docker pull natpdev/wasienv-compiler
 docker pull natpdev/wasi-service
+docker pull natpdev/poa-evm
+docker pull natpdev/nvidia-docker
