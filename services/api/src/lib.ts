94a8efba73bd866ab393020e96502e7ec9c20a6e
NatPDeveloper <ubuntu@ip-172-31-81-246.ec2.internal>
2022-06-16 19:57:39 +0000

    add pre check to lib

--- services/api/src/lib.ts
+++ services/api/src/lib.ts
@@ -26,3 +26,9 @@ export const toMegaBytes = (item) => {
         throw new Error(`should not get here: ${size}, ${base}`);
     }
 }
+
+export const preCheck = (approvedServices,image,res) => {
+    if(!image || !approvedServices.includes(image)) {
+        return res.status(401).send("Image doesn't exist or not supported");
+    }
+}
