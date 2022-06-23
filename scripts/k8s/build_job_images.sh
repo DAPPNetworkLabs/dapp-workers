9c5cd4faef5805d93307bf2b6c0275d372069811
NatPDeveloper <ubuntu@ip-172-31-81-246.ec2.internal>
2022-06-23 11:29:20 +0000

    add no cache arg, output only errors docker build/push

--- scripts/k8s/build_job_images.sh
+++ scripts/k8s/build_job_images.sh
@@ -1,17 +1,16 @@
 eval $(minikube docker-env)
 read -p 'Push boxes to docker hub? t/f: ' push_docker_hub
 for file in $PWD/images/* ; do
-    echo $file
     f="$(basename -- $file)"
     echo $f
-    docker build -t natpdev/$f ./images/$f
+    docker build --no-cache -t natpdev/$f ./images/$f > /dev/null
     if test "$push_docker_hub" = "t"
-        then docker push natpdev/$f
+        then docker push natpdev/$f | grep digest
     fi
 done
 
-docker build -t natpdev/rust-compiler ./images/compiler/rust
-docker build -t natpdev/wasienv-compiler ./images/compiler/wasienv
+docker build --no-cache -t natpdev/rust-compiler ./images/compiler/rust > /dev/null
+docker build --no-cache -t natpdev/wasienv-compiler ./images/compiler/wasienv > /dev/null
 if test "$push_docker_hub" = "t"
-    then docker push natpdev/rust-compiler; docker push natpdev/wasienv-compiler
+    then echo "rust-compiler"; docker push natpdev/rust-compiler | grep digest; echo "wasienv-compiler"; docker push natpdev/wasienv-compiler | grep digest
 fi
