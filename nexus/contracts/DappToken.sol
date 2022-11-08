69f8ad9696334159b2dfc4c26c56287bf85f93cc
NatPDeveloper <natanael.prudhomme@gmail.com>
2022-11-08 14:48:52 -0400

    name contract DAPP Token

--- nexus/contracts/DappToken.sol
+++ nexus/contracts/DappToken.sol
@@ -5,7 +5,7 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract DAPP is ERC20, Ownable, ERC20Permit {
+contract DAPPToken is ERC20, Ownable, ERC20Permit {
     
     address public minter;
    
