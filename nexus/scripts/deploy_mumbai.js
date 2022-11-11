dbfb6e4a362a3b8a6d005f08245392d9b64dfeee
NatPDeveloper <natanael.prudhomme@gmail.com>
2022-11-11 12:09:00 -0400

    update deploy script

--- nexus/scripts/deploy_mumbai.js
+++ nexus/scripts/deploy_mumbai.js
@@ -4,6 +4,8 @@ const delay = s => new Promise(res => setTimeout(res, s*1000));
 const workerGasPremium = 700000000;
 const fallbackGasPrice = 1000000000000;
 const stalenessSeconds = 86400;
+const lastUsdDappPrice = 9009009;
+const lastDappMaticPrice = 1186277653093940;
 const ipfsHash = "QmPdhPDtAruFXZCzBZBhMeYfyMPGd4WPKRazbmKVcF3QWW";
 
 const deploymentAddress = "0x39396235081A7F3372C3D74b52C41fab90444E48";
@@ -17,8 +19,8 @@ async function main() {
 
     const dappTokenContract = await dappTokenFactory.deploy(deploymentAddress);
     const dappOracleContract = await oracleFactory.deploy(
-      9009009, // 1 / $0.00111
-      1186277653093940 // (0.00111 / 0.9357) * 1e18 -> (1186277653093940 / 1e18) * .92 = 0.00109137544085
+      lastUsdDappPrice, // 1 / $0.00111
+      lastDappMaticPrice // (0.00111 / 0.9357) * 1e18 -> (1186277653093940 / 1e18) * .92 = 0.00109137544085
     );
 
     const nexusProxyContract = await upgrades.deployProxy(nexusTokenFactory, 
@@ -49,21 +51,43 @@ async function main() {
     console.log(`oracle contract: ${dappOracleContract.address}`);
     
     console.log("wait 60s for etherscan backend to catch up");
-    await delay(60);
+    await delay(120);
     
     console.log("verifying on etherscan...");
-    await hre.run("verify:verify", {
+    try {
+      await hre.run("verify:verify", {
         address: nexusContract,
         constructorArguments: []
-    });
-    await hre.run("verify:verify", {
-        address: dappTokenContract.address,
-        constructorArguments: []
-    });
-    await hre.run("verify:verify", {
-        address: dappOracleContract.address,
-        constructorArguments: []
-    });
+      });
+    } catch(e) {
+      console.log(`issue with nexus`)
+      console.log(e)
+    }
+    await delay(2);
+    try {
+      await hre.run("verify:verify", {
+          address: dappTokenContract.address,
+          constructorArguments: [
+            deploymentAddress
+          ]
+      });
+    } catch(e) {
+      console.log(`issue with token`)
+      console.log(e)
+    }
+    await delay(2);
+    try {
+      await hre.run("verify:verify", {
+          address: dappOracleContract.address,
+          constructorArguments: [
+            lastUsdDappPrice,
+            lastDappMaticPrice
+          ]
+      });
+    } catch(e) {
+      console.log(`issue with oracle`)
+      console.log(e)
+    }
 }
 
 main()
